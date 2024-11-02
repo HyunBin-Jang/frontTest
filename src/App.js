6d0afe6c963df4607da50b45753b649a823a6eb7
Jang HyunBin <janghb3730@gmail.com>
2024-11-02 17:44:33 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
+function selectData(){
+  axios.post('/testData',["가","나","다"])
+      .then(function (res){
+        console.log(res)
+      });
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <button onClick={() => selectData()}>조회</button>
+        </div>
       </header>
     </div>
   );
